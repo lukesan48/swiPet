2489dc125063f119f6cd48a70ac622d9be2593e6
AnimeKay <matthewchen17@gmail.com>
2024-07-08 00:14:26 -0400

    New and updated api's
    Added email verification & forgot password api

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongodb = require('mongodb');
 const bcrypt = require('bcrypt');
-const {ObjectId} = require('mongodb');
+const { ObjectId } = require('mongodb');
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
 require('mongodb');
 require('express');
 
@@ -30,18 +30,26 @@ exports.setApp = function (app, client) {
 
         // A user's login is found
         if (user) {
-            const passwordMatch = await bcrypt.compare(password, user.Password);
 
-            // Password matches
-            if (passwordMatch) {
-                ret = { id: user._id, firstName: user.FirstName, lastName: user.LastName, message: message };
-            }
+            // Check to see if email verified
+            if (user.Verified) {
+                const passwordMatch = await bcrypt.compare(password, user.Password);
 
-            // Password doesn't match
-            else {
-                message = "Invalid credentials";
+                // Password matches
+                if (passwordMatch) {
+                    ret = { id: user._id, firstName: user.FirstName, lastName: user.LastName, message: message };
+                }
+
+                // Password doesn't match
+                else {
+                    message = "Invalid credentials";
+                    ret = { message: message };
+                }
+            } else {
+                message = "Please verify your email before logging in";
                 ret = { message: message };
             }
+
         }
         // User's login not found
         else {
@@ -86,7 +94,7 @@ exports.setApp = function (app, client) {
                 }
             }
             else {
-                message = "Invalid user";
+                message = "User not found";
             }
         } catch (e) {
             message = e.toString();
@@ -96,18 +104,105 @@ exports.setApp = function (app, client) {
         res.status(200).json(ret);
     });
 
-    // Update user api
+    // Update user api - not including password
     app.post("/api/updateUser", async (req, res, next) => {
+        // incoming: login, firstName, lastName, email, phoneNumber, location
+        // outgoing: (new/same - firstName, lastName, email, phoneNumber, location), message
+        const { login, firstName, lastName, email, phoneNumber, location } = req.body;
 
+        let message = '';
+
+        const db = client.db(dbName);
+        const collection = db.collection('User');
+
+        let user = await collection.findOne({ Login: login });
+
+        // Check to see if valid user
+        if (user) {
+
+            let updatedUser = { FirstName: firstName, LastName: lastName, Email: email, PhoneNumber: phoneNumber, Location: location };
+
+            // Trim empty fields from updatedUser
+            // Done by stringify, which does not stringify
+            // undefined data, and parsing back to json
+            updatedUser = JSON.parse(JSON.stringify(updatedUser));
+
+
+            try {
+                // updateOne (filter, update)
+                // Filter based off login
+                const result = await collection.updateOne(
+                    // Case sensitive; Login: login,
+                    // not login: login
+                    { Login: login },
+                    { $set: updatedUser }
+                )
+
+                // Check to see if anything was updated
+                if (result.modifiedCount === 0) {
+                    message = "No changes made to user";
+                }
+                else {
+                    message = "User updated successfully";
+                }
+
+            } catch (e) {
+                message = e.toString();
+            }
+
+        }
+        else {
+            message = "User not found";
+        }
+
+        let ret = { message: message };
+        res.status(200).json(ret);
     });
 
     // Forgot password api
     app.post("/api/forgotPassword", async (req, res, next) => {
+        // incoming: login, email, newPassword
+        // outgoing: newPassword
+        const { login, email, newPassword } = req.body;
+        let message = '';
 
+        const db = client.db(dbName);
+        const collection = db.collection('User');
+
+        // Find based off email AND login, incase same email
+        let user = await collection.findOne({ Login: login, Email: email });
+        // console.log(user);
+
+        if (user) {
+            if (user.Verified) {
+                // Make sure to hash new password
+                const saltRounds = 12;
+                const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
+
+                // Filter based off login, since unique
+                const result = await collection.updateOne(
+                    { Login: login },
+                    { $set: { Password: hashedPassword } }
+                );
+
+                message = 'Password updated successfully';
+            }
+            else {
+                message = 'Please verify your email before changing password';
+            }
+        }
+        else {
+            message = 'No such login/email';
+        }
+
+
+        let ret = { message: message };
+        res.status(200).json(ret);
     });
 
     // Register api
-    // Need to implement password hashing via bcrypt - to do
+    // Need to implement password hashing via bcrypt - done
+    // Need to implement email verification - done
     app.post("/api/register", async (req, res, next) => {
         // incoming: firstName, lastName, login, password
         // outgoing: id, firstName, lastName, email, message
@@ -123,14 +218,14 @@ exports.setApp = function (app, client) {
         try {
             // findOne finds one instance, which there should only be one
             // instance of a login anyways
-            const existingUser = await db.collection("User").findOne({ Login: login });
+            const existingUser = await db.collection('User').findOne({ Login: login });
 
             // If user is found, don't do anything
             if (existingUser) {
                 message = "User already exists..."
             }
             else {
-                
+
                 //== bCrypt stuff... ==
                 // Essentially determines how long is spent
                 // on hashing password; higher is better, but
@@ -142,19 +237,48 @@ exports.setApp = function (app, client) {
 
                 // No user with login found, so make new user
                 const newUser = {
-                    FirstName: firstName, 
-                    LastName: lastName, 
+                    FirstName: firstName,
+                    LastName: lastName,
                     Email: email,
                     PhoneNumber: phoneNumber,
-                    Location: location, 
-                    Login: login, 
-                    Password: hashedPassword, 
-                    Favorites: [], 
-                    Listings: []
+                    Location: location,
+                    Login: login,
+                    Password: hashedPassword,
+                    Favorites: {},
+                    Listings: {},
+                    Verified: false
                 };
 
-                const result = await db.collection("User").insertOne(newUser);
+                const result = await db.collection('User').insertOne(newUser);
                 id = result.insertedId;
+
+                // Generate email verification token
+                const token = jwt.sign({ userId: id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+                // Setting up email for sending verification...
+                const transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: process.env.EMAIL_USER,
+                        // Google's app password, not actual
+                        // password
+                        pass: process.env.EMAIL_PASS
+                    }
+                });
+
+                // The email itself
+                const mailOptions = {
+                    from: process.env.EMAIL_USER,
+                    to: email,
+                    subject: 'Email Verification for swiPet',
+                    // website format: http://domain/api/verify-email?token=${token}
+                    // in this case locally since testing
+                    // Need to use backtick here for ${token}
+                    text: `Please verfiy your swiPet account by clicking on the follow link... http://localhost:5000/api/verifyEmail?token=${token}`
+                };
+
+                await transporter.sendMail(mailOptions);
+                message = "Registration successful. Proceed to verify email."
             }
 
         } catch (e) {
@@ -165,85 +289,114 @@ exports.setApp = function (app, client) {
         let ret = { id: id, firstName: firstName, lastName: lastName, email: email, message: message }
         res.status(200).json(ret);
     });
-    
+
+    // verify email api
+    app.get('/api/verifyEmail', async (req, res, next) => {
+        const { token } = req.query;
+        const db = client.db(dbName);
+
+        let message;
+
+        try {
+
+            // Decode token and get userId from it
+            const decodedToken= jwt.verify(token, process.env.JWT_SECRET);
+            const userId = decodedToken.userId;
+
+            // Find userId and edit Verified boolean
+            // Need new ObjectId here
+            await db.collection('User').updateOne(
+                { _id: new ObjectId(userId) },
+                { $set: { Verified: true } }
+            );
+
+            message = 'Email verified successfully';
+        } catch (e) {
+            message = 'Invalid or expired token';
+        }
+
+        ret = { message : message };
+        res.status(200).json(ret);
+    });
+
     // API to add new pets to specific users and update their listings to reflect the new pet
     app.post('/api/addpet', async (req, res) => {
-        
+
         // incoming: userLogin, petName, type, petAge, petGender, breed, petSize, bio, contactEmail, location, images
         // outgoing: message, petId
-          
+
         const { userLogin, petName, type, petAge, petGender, breed, petSize, bio, contactEmail, location, images } = req.body;
         let message = '';
         let petId = null;
         try {
             // Connect to database
             const db = client.db(dbName);
-            
+
             // Checks if there is a valid user to create the pet, if there is then create the pet
             const user = await db.collection('User').findOne({ Login: userLogin });
             if (user) {
                 const newPet = {
-                Login: userLogin,
-                Pet_Name: petName,
-                Pet_Type: type,
-                Age: petAge,
-                Gender: petGender,
-                Breed: breed,
-                Size: petSize,
-                Bio: bio,
-                Contact_Email: contactEmail,
-                Location: location,
-                Images: images || []
-            };
-            // Insert new pet and their descriptions into database
-            const result = await db.collection('Pet').insertOne(newPet);
-                
-            // Needed to get the pet's ObjectId
-            petId = result.insertedId;
-                 
-            // Updates Listing of user who created the pet with the pet's ObjectId
-            await db.collection('User').updateOne(
-                { Login: userLogin },
-                { $push: { Listings: petId } }
-            );
-            message = "Pet Created";
+                    Login: userLogin,
+                    Pet_Name: petName,
+                    Pet_Type: type,
+                    Age: petAge,
+                    Gender: petGender,
+                    Breed: breed,
+                    Size: petSize,
+                    Bio: bio,
+                    Contact_Email: contactEmail,
+                    Location: location,
+                    Images: images || []
+                };
+                // Insert new pet and their descriptions into database
+                const result = await db.collection('Pet').insertOne(newPet);
+
+                // Needed to get the pet's ObjectId
+                petId = result.insertedId;
+
+                // Updates Listing of user who created the pet with the pet's ObjectId
+                await db.collection('User').updateOne(
+                    { Login: userLogin },
+                    { $push: { Listings: petId } }
+                );
+                message = "Pet Created";
             } else {
                 message = "User does not exist";
             }
         } catch (e) {
             message = e.toString();
-        }     
+        }
         const ret = { message: message, petId: petId };
         res.status(200).json(ret);
     });
 
     // API to add a pet to a user's favorites list
     app.post('/api/addfavorite', async (req, res) => {
-        
+
         // incoming: userLogin, petId
         // outgoing: message
-        
+
         const { userLogin, petId } = req.body;
         let message = '';
         try {
             // Connect to the database
             const db = client.db(dbName);
-            
+
             // Checks if there is a valid user
             const user = await db.collection('User').findOne({ Login: userLogin });
             if (user) {
                 const objectId = new ObjectId(petId);
-                
+
                 // Check if there is a valid pet as well
                 const pet = await db.collection('Pet').findOne({ _id: objectId });
-                
+
                 // If there is a valid user and a valid pet, add that pet to the user's favorited pets
                 if (pet) {
                     await db.collection('User').updateOne(
-                    { Login: userLogin },
-                    { $addToSet: { Favorites: objectId } } 
-                );
-                message = "Pet added to favorites";
+                        { Login: userLogin },
+                        { $addToSet: { Favorites: objectId } }
+                    );
+                    message = "Pet added to favorites";
                 } else {
                     message = "Pet not found";
                 }
@@ -259,22 +412,22 @@ exports.setApp = function (app, client) {
 
     // API to delete a pet and the listing of the original user who uploaded the pet (as well as from the favorites list of anyone who has that pet favorited)
     app.post('/api/deletepet', async (req, res) => {
-        
+
         // incoming: userLogin, petId
         // outgoing: message
-  
+
         const { userLogin, petId } = req.body;
         let message = '';
         try {
             // Connect to database
             const db = client.db(dbName);
             const user = await db.collection('User').findOne({ Login: userLogin });
-            
+
             // If there is a valid user, proceed to see if the pet is in the database
             if (user) {
                 const objectId = new ObjectId(petId);
                 const pet = await db.collection('Pet').findOne({ _id: objectId });
-                
+
                 // If the pet is in the database, proceed with deletion
                 if (pet) {
 
@@ -284,9 +437,9 @@ exports.setApp = function (app, client) {
                     } else {
                         // Delete from the pet collection
                         await db.collection('Pet').deleteOne({ _id: pet._id });
-                        
+
                         // Update the creator's listings to remove it
-                        await db.collection('User').updateOne(   
+                        await db.collection('User').updateOne(
                             { Login: userLogin },
                             { $pull: { Listings: pet._id } }
                         );
@@ -294,9 +447,9 @@ exports.setApp = function (app, client) {
                         // Update all favorite's lists in all users so that it reflects that the pet was deleted
                         await db.collection('User').updateMany(
                             { Favorites: pet._id },
-                            { $pull: { Favorites: pet._id } } 
+                            { $pull: { Favorites: pet._id } }
                         );
-                    message = "Pet deleted successfully and removed from all favorites";
+                        message = "Pet deleted successfully and removed from all favorites";
                     }
                 } else {
                     message = "Pet not found";
@@ -308,83 +461,7 @@ exports.setApp = function (app, client) {
             message = e.toString();
         }
         const ret = { message: message };
-        res.status(200).json(ret);    
+        res.status(200).json(ret);
     });
 
-}   res.status(200).json(ret);
-    });
 }
-
-
-
-// app.post('/api/addcard', async (req, res, next) => {
-//     // incoming: userId, color
-//     // outgoing: error
-
-//     const { userId, card } = req.body;
-
-//     const newCard = { Card: card, UserId: userId };
-//     var error = '';
-
-//     try {
-//         const db = client.db('COP4331Cards');
-//         const result = db.collection('Cards').insertOne(newCard);
-//     }
-//     catch (e) {
-//         error = e.toString();
-//     }
-
-//     cardList.push(card);
-
-//     var ret = { error: error };
-//     res.status(200).json(ret);
-// });
-
-
-// app.post('/api/login', async (req, res, next) => {
-//     // incoming: login, password
-//     // outgoing: id, firstName, lastName, error
-
-//     var error = '';
-
-//     const { login, password } = req.body;
-
-//     const db = client.db('COP4331Cards');
-//     const results = await db.collection('User').find({ Login: login, Password: password }).toArray();
-
-//     var id = -1;
-//     var fn = '';
-//     var ln = '';
-
-//     if (results.length > 0) {
-//         id = results[0].UserId;
-//         fn = results[0].FirstName;
-//         ln = results[0].LastName;
-//     }
-
-//     var ret = { id: id, firstName: fn, lastName: ln, error: '' };
-//     res.status(200).json(ret);
-// });
-
-
-// app.post('/api/searchcards', async (req, res, next) => {
-//     // incoming: userId, search
-//     // outgoing: results[], error
-
-//     var error = '';
-
-//     const { userId, search } = req.body;
-
-//     var _search = search.trim();
-
-//     const db = client.db('COP4331Cards');
-//     const results = await db.collection('Cards').find({ "Card": { $regex: _search + '.*', $options: 'i' } }).toArray();
-
-//     var _ret = [];
-//     for (var i = 0; i < results.length; i++) {
-//         _ret.push(results[i].Card);
-//     }
-
-//     var ret = { results: _ret, error: error };
-//     res.status(200).json(ret);
-// });
