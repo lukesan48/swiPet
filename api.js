e449fef2aec7d13622df38602bb3eb4d8953d5a4
Luke Samuel Sandoval <lukesandoval@ucf.edu>
2024-07-25 19:43:42 -0400

    Update api.js
    fixed searchpet to filter out listings of user

--- api.js
+++ api.js
@@ -803,7 +803,7 @@ exports.setApp = function (app, client) {
 
         const { userLogin, type, petAge, petGender, colors, breed, petSize, location, jwtToken } = req.body;
         let message = '';
-
+        
         if (token.isExpired(jwtToken)) {
             let ret = { message: 'The JWT is no longer valid', jwtToken: jwtToken };
             res.status(200).json(ret);
@@ -813,18 +813,25 @@ exports.setApp = function (app, client) {
         try {
             // Connect to database
             const db = client.db(dbName);
-
-            // Used to find the user so that we can check their favorites list
-            const user = await db.collection('User').findOne({ username: userLogin });
+    
+    // Used to find the user so that we can check their favorites list
+    const user = await db.collection('User').findOne({ username: userLogin });
             if (!user) {
-                return res.status(200).json({ message: "User not found" });
+            return res.status(200).json({ message: "User not found" });
             }
-            // Checks for pets in the user's favorites list so that they don't show up in the search
-            const userFavorites = user.Favorites.map(favorite => new ObjectId(favorite));
+    // Checks for pets in the user's favorites list so that they don't show up in the search
+    const userFavorites = user.Favorites.map(favorite => new ObjectId(favorite));
+    const userListings = user.Listings.map(listing => new ObjectId(listing));
 
             // Make sure to get user login so it does not display user's listed pets
             // Make sure the fields are inputted, if not then ignore
-            let search = { Login: { $ne: userLogin }, _id: { $nin: userFavorites } };
+            let search = { 
+                Login: { $ne: userLogin }, 
+                _id: { 
+                    $nin: [...userFavorites, ...userListings] // Exclude both favorites and user's listings
+                }
+            };
+    
 
             if (type != "") search.Pet_Type = type;
             if (petAge != "") search.Age = petAge;
@@ -832,19 +839,19 @@ exports.setApp = function (app, client) {
             if (breed != "") search.Breed = breed;
             if (petSize != "") search.Size = petSize;
             if (location != "") search.Location = location;
-
+      
             // If the color is part of the allowed colors, search for pets of that color
             if (Array.isArray(colors) && colors.length > 0) {
                 const validColors = colors.filter(color => allowedColors.includes(color));
                 if (validColors.length > 0) {
                     // Searches for the color
-                    search.Color = { $in: validColors };
-                }
-            }
-
+                    search.Color = { $in: validColors }; 
+                } 
+            } 
+      
             // Search using the fields provided
             const pets = await db.collection('Pet').find(search).toArray();
-            if (pets.length === 0) {
+            if (pets.length === 0){
                 message = "No pets found";
             } else {
                 message = "Pets retrieved successfully";
@@ -855,7 +862,6 @@ exports.setApp = function (app, client) {
             message = e.toString();
         }
     });
-
 
     // pet inquiry api
     app.post('/api/sendInquiry', async (req, res, next) => {
